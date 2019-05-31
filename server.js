f1bff2fe15b209618cf16a8cbb75aaf68e0e2e52
Josue <josue.rivera@partnerhero.com>
2019-05-30 22:52:13 -0600

    Added User Model

--- server.js
+++ server.js
@@ -4,13 +4,44 @@ const express = require('express');
 const app = express();
 const Sequelize = require('sequelize');
 
+
 /* Database Setup */
 const dbClient = new Sequelize(process.env.POSTGRE_URI);
 
 
-// Testing the database connection
+/* Models */
+const User = dbClient.define('user', {
+    email: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        isUnique: true,
+        allowNull: false,
+        field: 'email'
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'hashed_password'
+    },
+    firstName: {
+        type: Sequelize.STRING,
+        field: 'first_name'
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        field: 'last_name'
+    },
+    budget: {
+        type: Sequelize.INTEGER,
+        field: 'budget'
+    }
+}, {
+    timestamps: false
+})
+
+
 dbClient.authenticate().then(() => {
-    console.log("Connection enabled");
+    console.log("Connection established");
 }).catch((err) => {
     console.log(err);
 });
