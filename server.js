b65fa21d8fd63ef39fced536a995d27ae39ea6e5
Josue <josue.rivera@partnerhero.com>
2019-06-02 19:21:15 -0600

    Enabled pass cors for all sites

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const tokenUtils = require("./utils/tokens.js");
 
 // Without this, the web application is not able to interact with the server
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
